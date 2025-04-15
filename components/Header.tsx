fed4169a0db42c11a5f75bc8c584a458958c0bba
Vick Kumar <vick@stiegleredtech.org>
2025-04-15 18:15:33 -0400

    changed the style

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,11 +8,11 @@ import { SidebarTrigger } from "./ui/sidebar"
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
-      <div className="max-w-screen-xl mx-auto flex h-16 items-center justify-between">
+      <div className="w-full mx-auto flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
-        <SidebarTrigger className="mr-2">
-                      <span className="sr-only">Open sidebar</span>
-                    </SidebarTrigger>
+          <SidebarTrigger className="mr-2">
+            <span className="sr-only">Open sidebar</span>
+          </SidebarTrigger>
           <Logo />
         </div>
         <div className="flex items-center gap-4">
