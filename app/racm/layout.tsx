fc102a7b55bfabd6e07b3b89b7d61c6fddaa5e9c
Vick Kumar <vick@stiegleredtech.org>
2025-04-15 18:13:46 -0400

    added racm toolbar

--- app/racm/layout.tsx
+++ app/racm/layout.tsx
@@ -1,14 +1,18 @@
 import AuditSelector from "@/components/AuditSelector"
+import RACMToolbar from "@/components/RACMToolbar"
 
 export default function RACMLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode
-  }>) {
-    return (
-        <div className="p-4">
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      <RACMToolbar />
+      <div className="p-4">
         <AuditSelector />
         {children}
-        </div>
-    )
-  }
+      </div>
+    </>
+  )
+}
