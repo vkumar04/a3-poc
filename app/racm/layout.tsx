47beb42ecedc78de0347e597093f58ffc2cb8f47
Vick Kumar <vick@stiegleredtech.org>
2025-04-15 17:36:07 -0400

    created racm page

---
+++ app/racm/layout.tsx
@@ -0,0 +1,12 @@
+export default function RACMLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode
+  }>) {
+    return (
+        <>
+        <div>Hello World</div>
+        {children}
+        </>
+    )
+  }
